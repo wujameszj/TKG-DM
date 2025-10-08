6b080e85812e82f7f21d12be481a373a1a4c0e31
James ZJ Wu <22258235+wujameszj@users.noreply.github.com>
2025-10-08 15:07:35 +0900

    simplify gaussian generation

--- utils.py
+++ utils.py
@@ -37,22 +37,27 @@ def channel_mean_shift(z_T: torch.Tensor, target_shift: float = 0.11) -> torch.T
         z_T_star[:, c, :, :] = shifted
     return z_T_star
 
+
 def create_2d_gaussian(height, width, std_dev, center_x=0, center_y=0):
     """
     Create a 2D Gaussian distribution where the center (center_x, center_y) is 1 and the periphery gradually decays towards 0.
     Coordinates are normalized in the range [-1, 1] using the specified standard deviation (std_dev).
     Returns a tensor with shape (1, 1, height//8, width//8).
     """
-    y = torch.linspace(-1, 1, height // 8)
-    x = torch.linspace(-1, 1, width // 8)
+#     y = torch.linspace(-1, 1, height // 8)
+#     x = torch.linspace(-1, 1, width // 8)
+    y = torch.linspace(-1, 1, height)
+    x = torch.linspace(-1, 1, width)
     y_grid, x_grid = torch.meshgrid(y, x, indexing="ij")
 
     x_grid = x_grid - center_x
     y_grid = y_grid - center_y
 
     gaussian = torch.exp(-((x_grid ** 2 + y_grid ** 2) / (2 * std_dev ** 2)))
-    gaussian = torch.nn.functional.interpolate(gaussian.unsqueeze(0).unsqueeze(0), scale_factor=8, mode='bilinear', align_corners=False)
-    return gaussian
+    return gaussian.unsqueeze(0).unsqueeze(0)
+#     gaussian = torch.nn.functional.interpolate(gaussian.unsqueeze(0).unsqueeze(0), scale_factor=8, mode='bilinear', align_corners=False)
+#     return gaussian
+
 
 def tkg_noise(latents: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
     """
